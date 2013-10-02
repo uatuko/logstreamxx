20343d6428ba788f8cb7bfaa6e74e429a730f4f9
Uditha Atukorala <udi@geniusse.com>
2013-10-02 01:04:38 +0100

    adding source examples

---
+++ src/examples/stdout_logger.cpp
@@ -0,0 +1,35 @@
+/*
+*  logstreamxx - C++ logging library based on standard stream classes
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#include <logstreamxx/logstream.h>
+#include <logstreamxx/priority.h>
+
+
+int main() {
+
+	logstreamxx::logstream logger;
+	logger << "24 in hex: " << std::hex << 24 << std::endl;
+	logger << "24 in oct: " << std::oct << 24 << std::endl;
+
+	logger << logstreamxx::priority::info << "this is info level" << std::endl;
+
+	return 0;
+
+}
+
