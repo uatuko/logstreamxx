ed1a92133434bf214c723912fa438100031ad2fb
Uditha Atukorala <udi@geniusse.com>
2013-10-05 12:59:47 +0100

    first set of unit tests for log stream buffer class

---
+++ test/logstreambuf_test.cpp
@@ -0,0 +1,41 @@
+/*
+*  logstreamxx - C++ logging library based on standard stream classes
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#include "logstreambuf_test.h"
+
+#include <logstreamxx/logstreambuf.h>
+
+
+// register the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( logstreambuf_test );
+
+// use namespace logstreamxx
+using namespace logstreamxx;
+
+
+void logstreambuf_test::test_constructor() {
+
+	// log stream buffer
+	logstreambuf sb;
+
+	// assert - read buffer will be empty always
+	CPPUNIT_ASSERT( 0 == sb.in_avail() );
+
+}
+
