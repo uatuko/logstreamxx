eb1e3bff45bb00da4698e3e5f65ac2e118ff9e65
Uditha Atukorala <udi@geniusse.com>
2013-10-10 20:30:43 +0100

    adding example code to log to a file

---
+++ src/examples/file_logger.cpp
@@ -0,0 +1,46 @@
+/*
+*  logstreamxx - C++ logging library based on standard stream classes
+*  Copyright (C) 2013 Uditha Atukorala
+*
+*  This software library is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This software library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this software library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#include <logstreamxx/logstream.h>
+#include <logstreamxx/priority.h>
+#include <string>
+
+
+int main( int argc, char* argv[] ) {
+
+	// log output file
+	std::string log_file = "/tmp/logstreamxx.log";
+
+	// log stream
+	logstreamxx::logstream logger( log_file.c_str() );
+
+	// set log output level
+	logger.loglevel( logstreamxx::priority::debug );
+
+	logger << "default is to log at debug level" << std::endl;
+	logger << "24 in hex: " << std::hex << 24 << std::endl;
+	logger << "24 in oct: " << std::oct << 24 << std::endl;
+
+	logger << logstreamxx::priority::info << "this is logged at info level" << std::endl;
+	logger << "this will also be in info level" << std::endl;
+
+	return 0;
+
+}
+
